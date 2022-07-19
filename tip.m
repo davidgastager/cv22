13e8c5a2bcdfc49880ac730ca65f0b5ad0b6537f
David Gastager <david.gastager@tum.de>
2022-07-19 15:55:08 +0200

    Merge Yan & Shis Updates

--- tip.m
+++ tip.m
@@ -364,29 +364,43 @@ function fig = tip(img, vp, p7, p2, varargin)
     cf=gcf;
     axis off;
     
+%% Add Buttons for easier Control
     % Orbit button
-    orbitbutton=uicontrol(cf,'style','Pushbutton','String','Orbit','Position',[20 20 70 30],'Callback',@orbit_callback)
+    orbitbutton=uicontrol(cf,'style','Pushbutton','String','Orbit','Position',[10 20 70 30],'Callback',@orbit_callback)
     function orbit_callback(~,~)
         cameratoolbar('SetMode','orbit')
     end
     % Pan button
-    panbutton=uicontrol(cf,'style','Pushbutton','String','Pan','Position',[100 20 70 30],'Callback',@pan_callback);
+    panbutton=uicontrol(cf,'style','Pushbutton','String','Pan','Position',[85 20 70 30],'Callback',@pan_callback);
     function pan_callback(~,~)
         cameratoolbar('SetMode','pan')
     end
     % Forward/Back button
-    dollyfbbutton=uicontrol(cf,'style','Pushbutton','String','Forward/Back','Position',[180 20 90 30],'Callback',@dollyfb_callback);
+    dollyfbbutton=uicontrol(cf,'style','Pushbutton','String','Forward/Back','Position',[160 20 70 30],'Callback',@dollyfb_callback);
     function dollyfb_callback(~,~)
         cameratoolbar('SetMode','dollyfb')
     end
     % Zoom button
-    zoom=uicontrol(cf,'style','Pushbutton','String','Zoom','Position',[280 20 70 30],'Callback',@zoom_callback);
+    zoom=uicontrol(cf,'style','Pushbutton','String','Zoom','Position',[235 20 70 30],'Callback',@zoom_callback);
     function zoom_callback(~,~)
         cameratoolbar('SetMode','zoom')
     end
+    % Move button
+    move=uicontrol(cf,'style','Pushbutton','String','Move','Position',[310 20 70 30],'Callback',@move_callback);
+    function move_callback(~,~)
+        cameratoolbar('SetMode','dollyhv')
+    end
     % Perspective projection botton
-    perspective=uicontrol(cf,'style','Pushbutton','String','Perspective projection','Position',[360 20 170 30],'Callback',@perspective_callback);
+    perspective=uicontrol(cf,'style','Pushbutton','String','Perspective','Position',[385 20 70 30],'Callback',@perspective_callback);
     function perspective_callback(~,~)
         camproj('perspective')
     end
+    % Orthographic projection botton
+    orthographic=uicontrol(cf,'style','Pushbutton','String','Orthographic','Position',[460 20 70 30],'Callback',@orthographic_callback);
+    function orthographic_callback(~,~)
+        camproj('orthographic')
+    end
+
+
+
 end
